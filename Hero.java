4b116c7b868f3386b3bb3f123544d6debf1b78d1
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2024-09-16 11:44:45 -0400

    upadte

--- Hero.java
+++ Hero.java
@@ -1,10 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Hero here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * What is going on?
  */
 public class Hero extends Actor
 {

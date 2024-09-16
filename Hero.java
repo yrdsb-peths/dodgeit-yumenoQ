c88366b7c0788b5a91958dc432da901cc9cb2b01
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2024-09-16 12:01:05 -0400

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,11 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-// New comment
 public class Hero extends Actor
 {
     public void act()
     {
         // Add your action code here.
-        move(1);
     }
 }

0b45674a958d06fd19bdd6c27db0aac74e9b2a74
Yumeno kyusaku <134026724+yumenoQ@users.noreply.github.com>
2024-09-30 13:08:02 -0400

    Delete Hero.java

--- Hero.java
+++
@@ -1,9 +0,0 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-public class Hero extends Actor
-{
-    public void act()
-    {
-        // Add your action code here.
-    }
-}
